e65a995a8042c36d8d170a51d378995a2c910bfc
Vanna <vanessa.km7@gmail.com>
2023-01-29 12:15:39 -0300

    adicionando funcionalidades

---
+++ script.js
@@ -0,0 +1,31 @@
+
+// CARD 1
+document.getElementById("c1").onmouseover = function() {mouseOver1()};
+document.getElementById("c1").onmouseout = function() {mouseOut1()};
+
+function mouseOver1() {document.getElementById("bb1").style.visibility = 'visible'};
+function mouseOut1() {document.getElementById("bb1").style.visibility = 'hidden'};
+
+// CARD 2
+document.getElementById("c2").onmouseover = function() {mouseOver2()};
+document.getElementById("c2").onmouseout = function() {mouseOut2()};
+
+function mouseOver2() {document.getElementById("bb2").style.visibility = 'visible'};
+function mouseOut2() {document.getElementById("bb2").style.visibility = 'hidden'};
+
+// CARD 3
+document.getElementById("c3").onmouseover = function() {mouseOver3()};
+document.getElementById("c3").onmouseout = function() {mouseOut3()};
+
+function mouseOver3() {document.getElementById("bb3").style.visibility = 'visible'};
+function mouseOut3() {document.getElementById("bb3").style.visibility = 'hidden'};
+
+
+// CARD 4
+document.getElementById("c4").onmouseover = function() {mouseOver4()};
+document.getElementById("c4").onmouseout = function() {mouseOut4()};
+
+function mouseOver4() {document.getElementById("bb4").style.visibility = 'visible'};
+function mouseOut4() {document.getElementById("bb4").style.visibility = 'hidden'};
+
+
